e25d98b3c3289f8fda51a0a79c7e475169b318a9
ankusharoraa <prat9912@gmail.com>
2020-09-10 16:11:15 +0530

    final commit with logo changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import logo from '../images/cgLogo.png'
 export default function Footer(props){
     let date = new Date();
     let year = date.getFullYear()
 
 return(
     <div className = "footer">
-        <p>© Copyright Co-Working Space {year}</p>
+        <p>© Capgemini <img src = {logo} alt = "Cg logo" style = {{height : '20px', width : '20px'}}></img> {year}. All rights reserved</p>
     </div>
 )
 }
