e26036b1d9b345c681086158000c15b74149148b
ankusharoraa <prat9912@gmail.com>
2020-10-27 20:35:22 +0530

    Initial commit

---
+++ build/precache-manifest.0e3614a67bc5ec9caae4c251016a4a39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfc8ad938f779fc207ff7c129aef193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7729d88c6b975db13974",
+    "url": "/static/css/2.9c11037e.chunk.css"
+  },
+  {
+    "revision": "af71d3bfaa44932ceedf",
+    "url": "/static/css/main.b389b6a9.chunk.css"
+  },
+  {
+    "revision": "7729d88c6b975db13974",
+    "url": "/static/js/2.4de9fc1a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.4de9fc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af71d3bfaa44932ceedf",
+    "url": "/static/js/main.b2d96742.chunk.js"
+  },
+  {
+    "revision": "6752291f9afda420601d",
+    "url": "/static/js/runtime-main.cb2317f9.js"
+  },
+  {
+    "revision": "872705a72d2d76cc6c7a0799ee02b2c2",
+    "url": "/static/media/cgLogo.872705a7.png"
+  },
+  {
+    "revision": "69f47c8c0db1236e30d4ccb22df24615",
+    "url": "/static/media/coWorking3.69f47c8c.jpg"
+  },
+  {
+    "revision": "64531772aa156f0185b24cc411812937",
+    "url": "/static/media/logo.64531772.jpg"
+  }
+]);
